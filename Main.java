3662da3821a0e46d50ac4934b83a13fa89dec484
bwhitmore7 <bryson.whitmore@patelhs.org>
2020-11-03 22:49:25 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,25 @@
+public class Main {
+  public static void main(String[] args) {
+    String[] food = { "pizza", "cake", "hot dog", "tacos", "sushi", "chicken", "steak", "burger", "sandwhich" };
+
+    for (int i = 0; i < 9; i++) {
+      System.out.println(food[i]);
+    }
+
+    food[6] = "fish";
+
+    System.out.println(" \n heres my updated array: \n");
+
+    for (int i = 0; i < 9; i++) {
+      System.out.println(food[1]);
+    }
+
+  String[] i = {"1","2","3","4","5","6","7","8","9"};
+    for (int i = 8; i > 0; i--) {
+      if (int[] % 2 == 0)
+        System.out.println(food[i]);
+
+    }
+    // food[3] % 2 == 0
+  }
+}
